d9524712f8e96ebc196ca3093f9b5f0539c15694
alex-ties-emma <alex.ties@attemic.com>
2025-08-16 18:57:00 +0200

    create automatically generated content

--- page-generator/generate-static-pages.js
+++ page-generator/generate-static-pages.js
@@ -4,7 +4,9 @@ import {writeFile} from 'fs/promises';
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
-let elementDefinitons = {};
+import crypto from "crypto";
+
+let elementDefinitions = {};
 
 let pageDefinitions = {}
 
@@ -13,13 +15,78 @@ let pageDefinitions = {}
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const data = JSON.parse(fs.readFileSync('../data/locationdata.json', 'utf8'));
+const locationdata = JSON.parse(fs.readFileSync('../data/locationdata.json', 'utf8'));
 const trentinoDistrictsData = JSON.parse(fs.readFileSync('../data/trentino-district-data.json', 'utf8'));
 const trentinoDistricts = JSON.parse(fs.readFileSync('../data/trentino-districts.json', 'utf8'));
 const geolocation = JSON.parse(fs.readFileSync('../data/geolocation.json', 'utf8'));
 
 const elementDefinitionfolderPath = path.join(__dirname, "../config/element-definitions");
 const pageDefinitionfolderPath = path.join(__dirname, "../config/page-definitions");
+
+
+let priceScale = [
+    { "step": 0, "hex": "#33691E", "label":"very_low" },
+    { "step": 1, "hex": "#5E841D", "label":"very_low"  },
+    { "step": 2, "hex": "#89A01C", "label":"low"  },
+    { "step": 3, "hex": "#B3BC1B", "label":"low" },
+    { "step": 4, "hex": "#DED71A", "label":"medium" },
+    { "step": 5, "hex": "#FFEB3B", "label":"medium" },
+    { "step": 6, "hex": "#FFB62D", "label":"medium" },
+    { "step": 7, "hex": "#FF8120", "label":"medium" },
+    { "step": 8, "hex": "#FF4C12", "label":"high" },
+    { "step": 9, "hex": "#FF1605", "label":"high" },
+    { "step": 10, "hex": "#C30003", "label":"very_high" },
+    { "step": 11, "hex": "#7B0000", "label":"very_high" }
+];
+
+const pricesValues = Object.values(locationdata).map(x => x.price);
+
+
+const min = Math.min(...pricesValues);
+const max = Math.max(...pricesValues);
+
+const steps = [];
+
+const stepSize = (max - min) / 11;
+
+for (let i = 0; i < 12; i++) {
+    const rawValue = min + stepSize * i;
+    const roundedUpTo50 = Math.ceil(rawValue / 50) * 50;
+    steps.push(roundedUpTo50);
+}
+
+Object.keys(locationdata).forEach((municipalityName) => {
+
+    let price = locationdata[municipalityName].price;
+
+    let index = getStepIndexForPrice(steps, price);
+
+    let priceStep = priceScale.find(x => x.step === index);
+
+    locationdata[municipalityName].priceValuation = {
+        color: priceStep.hex,
+        label: priceStep.label,
+
+    }
+
+});
+Object.keys(trentinoDistrictsData).forEach((districtName) => {
+
+    let price = trentinoDistrictsData[districtName].average;
+
+    let index = getStepIndexForPrice(steps, price);
+
+    let priceStep = priceScale.find(x => x.step === index);
+
+    trentinoDistrictsData[districtName].priceValuation = {
+        color: priceStep.hex,
+        label: priceStep.label,
+
+    }
+
+});
+
+
 
 fs.readdirSync(elementDefinitionfolderPath).forEach((file) => {
     if (file.endsWith(".json")) {
@@ -31,7 +98,7 @@ fs.readdirSync(elementDefinitionfolderPath).forEach((file) => {
             const id = data["@id"];
 
             if (handle && id) {
-                elementDefinitons[handle] = id;
+                elementDefinitions[handle] = id;
             }
         } catch (err) {
             console.warn(`Skipping invalid JSON file: ${file}`);
@@ -57,7 +124,7 @@ fs.readdirSync(pageDefinitionfolderPath).forEach((file) => {
     }
 });
 
-console.log({elementDefinitons, pageDefinitions})
+console.log({elementDefinitions, pageDefinitions})
 
 function generateUUID() {
     return crypto.randomUUID();
@@ -68,14 +135,11 @@ let sites = {
 }
 
 let languages = {
-
+    "DE": "\/cms\/api\/languages\/2ee8416f-7274-45c0-83e0-12fc6f3ef4f4",
+    "IT": "\/cms\/api\/languages\/7fb31631-fe93-4ea9-b8d9-cc09c32e4477",
+    "EN": "\/cms\/api\/languages\/d76b0d26-ea6d-4d83-bd88-99af344e8507"
 }
 
-let elementDefinitions = {
-    "content":"\/cms\/api\/section_element_definitions\/d68a7d34-6e0f-4477-9a03-f8aa47f592e5"
-
-
-}
 
 
 
@@ -114,7 +178,7 @@ function createPageLanguage(langShort) {
         "url": "\/",
         "headHTML": "",
         "redirectTo": null,
-        "language": langShort,
+        "language": languages[langShort],
         "titlePriority": 0,
         "descriptionPriority": 0,
         "itemIndex": 0,
@@ -164,3 +228,145 @@ function createContent(definitionHandle) {
         "hasTodo": false
     }
 }
+
+function getStepIndexForPrice(steps, price) {
+    for (let i = 0; i < steps.length; i++) {
+        if (price <= steps[i]) {
+            return i;
+        }
+    }
+    // If price is greater than all steps, return the last index
+    return steps.length - 1;
+}
+
+console.log(locationdata);
+
+
+Object.keys(locationdata).forEach((municipalityName) => {
+
+    let municipalityData = locationdata[municipalityName];
+
+    console.log({value: municipalityData});
+
+    let filename = 'municipality-' + municipalityData.slug + ".json";
+
+    let page = createPage("municipality", "development", municipalityData.slug);
+    page.internalName = municipalityName;
+
+    let district = trentinoDistrictsData[municipalityData.district];
+
+    let districtSlugPrefix = {
+        "DE": "/bezirk/",
+        "IT": "/distretto/",
+        "EN": "/district/"
+    }
+
+    let municipalitySlugPrefix = {
+        "DE": "/gemeinde/",
+        "IT": "/comune/",
+        "EN": "/municipality/"
+    }
+
+    Object.keys(municipalitySlugPrefix).forEach(language => {
+
+        let pageLanguage = createPageLanguage(language);
+
+        pageLanguage.title = municipalityData["pageMeta"]["page_title"][language];
+        pageLanguage.description = municipalityData["pageMeta"]["meta_description"][language];
+        pageLanguage.navName = municipalityName;
+        pageLanguage.url = municipalitySlugPrefix[language] + municipalityData.slug;
+
+        pageLanguage.settings = {
+            municipality: municipalityName,
+            population: municipalityData.population,
+            meters_above_sea_level: municipalityData.height,
+            price: {
+                price: municipalityData.price,
+                color: municipalityData.priceValuation.color,
+                label: municipalityData.priceValuation.label,
+            },
+            infrastructureTable: municipalityData.infrastructure,
+            descriptions: municipalityData.descriptions[language],
+            district: {
+                name: municipalityData.district,
+                referenceText: district.referenceText[language],
+                slug: districtSlugPrefix[language] + district.slug,
+                image: district.slug + ".jpg"
+            }
+        };
+
+        page.pageLanguages.push(pageLanguage);
+    })
+
+
+    let filepath = "../pages/" + filename;
+
+    fs.writeFileSync(filepath, JSON.stringify(page, null, 2));
+});
+
+Object.keys(trentinoDistrictsData).forEach((districtName) => {
+
+    let districtData = trentinoDistrictsData[districtName];
+
+    let filename = 'district-' + districtData.slug + ".json";
+
+    let page = createPage("municipality", "development", districtData.slug);
+    page.internalName = districtName;
+
+
+    let districtSlugPrefix = {
+        "DE": "/bezirk/",
+        "IT": "/distretto/",
+        "EN": "/district/"
+    }
+
+    let municipalitySlugPrefix = {
+        "DE": "/gemeinde/",
+        "IT": "/comune/",
+        "EN": "/municipality/"
+    }
+
+
+    Object.keys(districtSlugPrefix).forEach(language => {
+
+        let pageLanguage = createPageLanguage(language);
+
+        pageLanguage.title = districtData["pageMeta"]["page_title"][language];
+        pageLanguage.description = districtData["pageMeta"]["meta_description"][language];
+        pageLanguage.navName = districtName;
+        pageLanguage.url = districtSlugPrefix[language] + districtData.slug;
+
+        pageLanguage.settings = {
+            district: districtName,
+            price: {
+                average: districtData.average,
+                color: districtData.priceValuation.color,
+                label: districtData.priceValuation.label,
+            },
+            infoTable: districtData.infos,
+            descriptions: districtData.descriptions[language]
+        };
+
+        let municipalitiesData = [];
+
+        Object.keys(locationdata).forEach((key) => {
+            let municipality = locationdata[key];
+
+            municipalitiesData.push({
+                name: key,
+                slug: municipalitySlugPrefix[language] + municipality.slug
+            });
+        })
+
+
+        pageLanguage.settings.municipalities = municipalitiesData;
+
+
+        page.pageLanguages.push(pageLanguage);
+    })
+
+
+    let filepath = "../pages/" + filename;
+
+    fs.writeFileSync(filepath, JSON.stringify(page, null, 2));
+});
